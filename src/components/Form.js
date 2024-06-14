e33afc5e08109c7fa3a88f57671102d2187e02fd
Serhii Du <5sergeydu@gmail.com5>
2024-06-14 16:18:26 +0200

    made changes to the chat form

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,22 +1,37 @@
 import "./FormStyles.css"
-
-import React from 'react'
+import React, { useRef } from 'react'
+import emailjs from '@emailjs/browser'
 
 const Form = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm('service_z8099zu', 'template_yjzvfn5', form.current, {
+        publicKey: 'Si6tjz6jabeKhuLm2',
+      })
+      .then(
+        () => {
+          alert('SUCCESS!');
+        },
+        (error) => {
+          alert('FAILED...', error.text);
+        },
+      );
+  };
+
   return (
-    <div className="form">
-        <form>
+        <form className="form" ref={form} onSubmit={sendEmail}>
             <label>Your name</label>
-            <input type="text"></input>
+            <input type="text" name="user_name"/>
             <label>E-Mail</label>
-            <input type="email"></input>
-            <label>Subject</label>
-            <input type="text"></input>
+            <input type="email" name="user_email"/>
             <label>Message</label>
-            <textarea rows="6" placeholder="Type your message here"/>
-            <button className="btn">Submit</button>
+            <textarea name="message" rows="6" placeholder="Type your message here"/>
+            <button className="btn" type="submit">Submit</button>
         </form>
-    </div>
   )
 }
 
