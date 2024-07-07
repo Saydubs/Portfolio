ba543117a9e7eba31ba73137d55f3995780adfa2
Saydubs <5sergeydu5@gmail.com>
2024-07-07 21:48:21 +0200

    little update, and add a project

--- src/components/WorkCardData.js
+++ src/components/WorkCardData.js
@@ -2,16 +2,27 @@ import pro1 from "../assets/Screenshot_1.png"
 import pro2 from "../assets/Screenshot_2.png"
 import pro3 from "../assets/Screenshot_3.png"
 import pro4 from "../assets/Screenshot_4.png"
+import pro5 from "../assets/Screenshot_5.png"
 
 const ProjectCardData = [
+    {
+        imgsrc: pro5,
+        title: {
+            en: "Layout markup",
+            ge: "Layout markup"},
+        text: {
+            en: "I completed the layout for a PSD design using HTML5, CSS3, and JS. The project was developed with Bootstrap 4 to ensure mobile responsiveness.",
+            ge: "Ich habe das Layout für ein PSD-Design mit HTML5, CSS3 und JS abgeschlossen. Das Projekt wurde mit Bootstrap 4 entwickelt, um die mobile Reaktionsfähigkeit sicherzustellen."},
+        view: "https://saydubs.github.io/LaslesVPN/"
+    },
     {
         imgsrc: pro1,
         title: {
             en: "Layout markup",
             ge: "Layout markup"},
         text: {
-            en: "I completed the layout markup from PSD using HTML5, CSS3, and the LESS preprocessor. It was done on Bootstrap 4 with adaptation for mobile devices. I used Animate.CSS and WOW.JS (note: animations do not render on GitHub Pages). Additionally, it is possible to connect an SMTP server for sending emails through a form using PHP SMTP Mailer. A JQuery input mask has also been applied to the user contact input fields.",
-            ge: "Ich habe das Layout aus PSD mithilfe von HTML5, CSS3 und dem LESS-Präprozessor umgesetzt. Es wurde auf Bootstrap 4 angepasst und für mobile Geräte optimiert. Ich habe Animate.CSS und WOW.JS verwendet (Hinweis: Animationen werden auf GitHub Pages nicht dargestellt). Zusätzlich besteht die Möglichkeit, einen SMTP-Server für den Versand von E-Mails über ein Formular mit PHP SMTP Mailer zu verbinden. Die Eingabefelder für die Kontaktdaten der Benutzer wurden auch mit einer JQuery-Eingabemaske versehen."},
+            en: "I completed the layout markup from PSD using HTML5, CSS3, and the LESS preprocessor. It was done on Bootstrap 4 with adaptation for mobile devices. I used Animate.CSS and WOW.JS. ",
+            ge: "Ich habe das Layout aus PSD mithilfe von HTML5, CSS3 und dem LESS-Präprozessor umgesetzt. Es wurde auf Bootstrap 4 angepasst und für mobile Geräte optimiert. Ich habe Animate.CSS und WOW.JS verwendet"},
         view: "https://saydubs.github.io/Gipsolit/"
     },
     {
@@ -24,18 +35,9 @@ const ProjectCardData = [
             ge: "Der Kunde benötigte eine Überarbeitung eines Blocks, kleinere Anpassungen und eine Änderung des Logos. Die Änderungen wurden umgesetzt. Der Block Länder wurde modifiziert und für mobile Geräte angepasst."},
         view: "https://freelancehunt.com/showcase/work/pravki-po-verstke-na-sayte-https-work-ivs-llp-co/984285.html"
     },
+
     {
         imgsrc: pro3,
-        title: {
-            en: "Layout markup",
-            ge: "Layout markup"},
-        text: {
-            en: "I completed the layout markup from PSD using HTML5, CSS3, and the LESS preprocessor. It was done on Bootstrap 4 with adaptation for mobile devices. I used Animate.CSS and WOW.JS (note: animations do not render on GitHub Pages). Additionally, it is possible to connect an SMTP server for sending emails through a form using PHP SMTP Mailer. A JQuery input mask has also been applied to the user contact input fields.",
-            ge: "Ich habe das Layout aus PSD mithilfe von HTML5, CSS3 und dem LESS-Präprozessor umgesetzt. Es wurde auf Bootstrap 4 angepasst und für mobile Geräte optimiert. Ich habe Animate.CSS und WOW.JS verwendet (Hinweis: Animationen werden auf GitHub Pages nicht dargestellt). Zusätzlich besteht die Möglichkeit, einen SMTP-Server für den Versand von E-Mails über ein Formular mit PHP SMTP Mailer zu verbinden. Die Eingabefelder für die Kontaktdaten der Benutzer wurden auch mit einer JQuery-Eingabemaske versehen."},
-        view: "https://freelancehunt.com/showcase/work/moy-lichnyiy-sayt/977384.html"
-    },
-    {
-        imgsrc: pro4,
         title: {
             en: "Java Spring training project",
             ge: "Java Spring Lernprojekt"},
@@ -43,7 +45,17 @@ const ProjectCardData = [
             en: "I studied Java Spring and completed a training project using the following technologies: Spring Boot Starter Data JPA, Starter Mail, Starter Security, Starter Web, MySQL Connector, and Spring Security Test.",
             ge: "Ich habe Java Spring gelernt und ein Lernprojekt mit den folgenden Technologien abgeschlossen: Spring Boot Starter Data JPA, Starter Mail, Starter Security, Starter Web, MySQL Connector und Spring Security Test."},
         view: "https://github.com/Saydubs/Easysell"
-    }
+    },
+    {
+        imgsrc: pro4,
+        title: {
+            en: "Layout markup",
+            ge: "Layout markup"},
+        text: {
+            en: "I completed the layout markup from PSD using HTML5, CSS3, and the LESS preprocessor. It was done on Bootstrap 4 with adaptation for mobile devices. I used Animate.CSS and WOW.JS.",
+            ge: "Ich habe das Layout aus PSD mithilfe von HTML5, CSS3 und dem LESS-Präprozessor umgesetzt. Es wurde auf Bootstrap 4 angepasst und für mobile Geräte optimiert. Ich habe Animate.CSS und WOW.JS verwendet."},
+        view: "https://freelancehunt.com/showcase/work/moy-lichnyiy-sayt/977384.html"
+    },
 ];
 
 export default ProjectCardData
