839342c00d098df38b9c89b357e75db1565fc67e
Saydubs <5sergeydu5@gmail.com>
2024-07-15 20:40:53 +0200

    klein fix

---
+++ src/components/WorkCardData.js
@@ -0,0 +1,84 @@
+import pro1 from "../assets/Screenshot_1.png"
+import pro2 from "../assets/Screenshot_2.png"
+import pro3 from "../assets/Screenshot_3.png"
+import pro4 from "../assets/Screenshot_4.png"
+import pro5 from "../assets/Screenshot_5.png"
+import pro6 from "../assets/Screenshot_6.png"
+import pro7 from "../assets/Screenshot_7.png"
+
+const ProjectCardData = [    {
+        imgsrc: pro7,
+        title: {
+            en: "Layout markup",
+            ge: "Layout markup"},
+        text: {
+            en: "I completed the layout for a PSD design using HTML5, CSS3, and JS. The project was developed to ensure mobile responsiveness.",
+            ge: "Ich habe das Layout für ein PSD-Design mit HTML5, CSS3 und JS abgeschlossen. Das Projekt wurde entwickelt, um die mobile Reaktionsfähigkeit sicherzustellen."},
+        view: "https://saydubs.github.io/shop/"
+    },
+    {   
+        imgsrc: pro5,
+        title: {
+            en: "Layout markup",
+            ge: "Layout markup"},
+        text: {
+            en: "I completed the layout for a PSD design using HTML5, CSS3, and JS. The project was developed with Bootstrap 4 to ensure mobile responsiveness.",
+            ge: "Ich habe das Layout für ein PSD-Design mit HTML5, CSS3 und JS abgeschlossen. Das Projekt wurde mit Bootstrap 4 entwickelt, um die mobile Reaktionsfähigkeit sicherzustellen."},
+        view: "https://saydubs.github.io/LaslesVPN/"
+    },
+    {
+        imgsrc: pro1,
+        title: {
+            en: "Layout markup",
+            ge: "Layout markup"},
+        text: {
+            en: "I completed the layout markup from PSD using HTML5, CSS3, and the LESS preprocessor. It was done on Bootstrap 4 with adaptation for mobile devices. I used Animate.CSS and WOW.JS. ",
+            ge: "Ich habe das Layout aus PSD mithilfe von HTML5, CSS3 und dem LESS-Präprozessor umgesetzt. Es wurde auf Bootstrap 4 angepasst und für mobile Geräte optimiert. Ich habe Animate.CSS und WOW.JS verwendet"},
+        view: "https://saydubs.github.io/Gipsolit/"
+    },
+    {
+        imgsrc: pro6,
+        title: {
+            en: "2048 Game JavaScript",
+            ge: "2048 ein Spiel JavaScript"},
+        text: {
+            en: "This is a simple tile merging game where players can move tiles up, down, left, or right. When two tiles with the same value collide, they merge into a single tile with the sum of their values. The goal is to keep merging tiles to achieve the highest possible score before no more moves are possible.",
+            ge: "Dies ist ein einfaches Kachel-Merging-Spiel, bei dem Spieler Kacheln nach oben, unten, links oder rechts bewegen können. Wenn zwei Kacheln mit demselben Wert kollidieren, verschmelzen sie zu einer einzigen Kachel mit der Summe ihrer Werte. Das Ziel ist es, Kacheln immer wieder zu verschmelzen, um die höchstmögliche Punktzahl zu erreichen, bevor keine Züge mehr möglich sind."},
+        view: "https://saydubs.github.io/2048Game/"
+    },
+    {
+        imgsrc: pro2,
+        title: {
+            en: "Layout adjustments",
+            ge: "Anpassungen des Layouts auf der Website"},
+        text: {
+            en: "The client needed a block to be redone, minor adjustments to be made, and the logo to be changed. The changes were implemented. The 'Countries' block was modified and adapted for mobile devices.",
+            ge: "Der Kunde benötigte eine Überarbeitung eines Blocks, kleinere Anpassungen und eine Änderung des Logos. Die Änderungen wurden umgesetzt. Der Block Länder wurde modifiziert und für mobile Geräte angepasst."},
+        view: "https://freelancehunt.com/showcase/work/pravki-po-verstke-na-sayte-https-work-ivs-llp-co/984285.html"
+    },
+
+    {
+        imgsrc: pro3,
+        title: {
+            en: "Java Spring training project",
+            ge: "Java Spring Lernprojekt"},
+        text: {
+            en: "I studied Java Spring and completed a training project using the following technologies: Spring Boot Starter Data JPA, Starter Mail, Starter Security, Starter Web, MySQL Connector, and Spring Security Test.",
+            ge: "Ich habe Java Spring gelernt und ein Lernprojekt mit den folgenden Technologien abgeschlossen: Spring Boot Starter Data JPA, Starter Mail, Starter Security, Starter Web, MySQL Connector und Spring Security Test."},
+        view: "https://github.com/Saydubs/Easysell"
+    },
+    {
+        imgsrc: pro4,
+        title: {
+            en: "Layout markup",
+            ge: "Layout markup"},
+        text: {
+            en: "I completed the layout markup from PSD using HTML5, CSS3, and the LESS preprocessor. It was done on Bootstrap 4 with adaptation for mobile devices. I used Animate.CSS and WOW.JS.",
+            ge: "Ich habe das Layout aus PSD mithilfe von HTML5, CSS3 und dem LESS-Präprozessor umgesetzt. Es wurde auf Bootstrap 4 angepasst und für mobile Geräte optimiert. Ich habe Animate.CSS und WOW.JS verwendet."},
+        view: "https://freelancehunt.com/showcase/work/miy-osobistiy-sayt/977384.html"
+    },
+
+
+];
+
+export default ProjectCardData
